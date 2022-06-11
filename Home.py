e3f21fe8f06519d76473eaaf7323ae68021e0c12
phemiloyeAI <oyerindefemiloye@gmail.com>
2022-06-11 14:34:20 +0100

    changes

--- Home.py
+++ Home.py
@@ -1,13 +1,17 @@
 import streamlit as st
+import os
+
+img_path = "demo\image.jpg"
+video_path = "demo\\video.webm"
 
 st.title("A Crowd Counting Application.")
 
 st.write("***The result of a video processed by the app***")
-st.video("https://github.com/Roc4t-Team/crowd-counting-application/blob/main/demo/video.webm")
+st.video(video_path)
 
 st.write('---')
 st.write("***The result of an image processed by the app***")
-st.image("https://github.com/Roc4t-Team/crowd-counting-application/blob/main/demo/image.jpg")
+st.image(img_path)
 
 
 st.sidebar.header("About")
